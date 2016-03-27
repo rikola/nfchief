15ba08c16349eeab98ba597a021a98d9fea9f526
Eric <rikola2@gmail.com>
2016-03-27 16:14:08 -0500

    Upgrade to minitest-reporter

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,17 +1,21 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  
+
+	def setup
+		@base_title = "NFChief"
+	end
+
 	test 'should get index' do
 		get :index
 		assert_response :success
-		assert_select "title", "Home – NFChief"
+		assert_select "title", "Home – #{@base_title}"
 	end
 
 	test 'should get about' do
 		get :about
 		assert_response :success
-		assert_select "title", "About – NFChief"
+		assert_select "title", "About – #{@base_title}"
 	end
 
 end
