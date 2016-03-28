0b42b517c147f14e921a63cd74309b25a0ac4edf
Eric <rikola2@gmail.com>
2016-03-27 20:04:45 -0500

    Pimp out signup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -13,4 +13,21 @@ class UserTest < ActiveSupport::TestCase
 		assert_not duplicate_user.valid?
 	end
 
+	test "email addresses should be saved as lower-case" do
+		mixed_case_email = "Foo@ExAMPle.CoM"
+		@user.email = mixed_case_email
+		@user.save
+		assert_equal mixed_case_email.downcase, @user.reload.email
+	end
+
+	test "password should be present (nonblank)" do
+		@user.password = @user.password_confirmation = " " * 6
+		assert_not @user.valid?
+	end
+
+	test "password should have a minimum length" do
+		@user.password = @user.password_confirmation = "a" * 5
+		assert_not @user.valid?
+	end
+
 end
