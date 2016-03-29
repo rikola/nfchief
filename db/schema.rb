a40d917e1d148dcb0007d146b79b2ad4fa231db7
Eric <rikola2@gmail.com>
2016-03-29 11:11:11 -0500

    Configure PostgreSQL to work with Travis-CI

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160328030554) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "clients", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -43,6 +46,6 @@ ActiveRecord::Schema.define(version: 20160328030554) do
     t.string   "remember_digest"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
